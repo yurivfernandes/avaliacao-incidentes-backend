3abdec697df6b08a9fdf63dc48a30b732b97f7e8
yurivfernandes <yuri.viana.fernandes@gmail.com>
2025-02-25 22:08:16 -0300

    atualiza configuração do banco de dados para PostgreSQL com variáveis de ambiente

--- backend/app/settings.py
+++ backend/app/settings.py
@@ -90,25 +90,6 @@ WSGI_APPLICATION = "app.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    },
-    # Configuração para SQL Server (comentada para desenvolvimento)
-    # 'sql_server': {
-    #     'ENGINE': 'mssql',
-    #     'NAME': 'nome_do_banco',
-    #     'USER': 'usuario',
-    #     'PASSWORD': 'senha',
-    #     'HOST': 'xxxx',
-    #     'PORT': '',
-    #     'OPTIONS': {
-    #         'driver': 'ODBC Driver 17 for SQL Server',
-    #     },
-    # },
-}
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
@@ -208,3 +189,19 @@ REST_FRAMEWORK = {
 
 # Configurações do Django Filter
 FILTERS_DEFAULT_LOOKUP_EXPR = "icontains"
+
+# Database
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.getenv("DB_NAME"),
+        "USER": os.getenv("DB_USER"),
+        "PASSWORD": os.getenv("DB_PASSWORD"),
+        "HOST": os.getenv("DB_HOST"),
+        "PORT": os.getenv("DB_PORT", "5432"),
+        "OPTIONS": {
+            "options": "-c statement_timeout=10000",
+            "target_session_attrs": "read-write",
+        },
+    }
+}
