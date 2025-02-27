18b5824b749297f9d9edd7edbb32f1392533afe3
yurivfernandes <yuri.viana.fernandes@gmail.com>
2025-02-27 16:24:01 -0300

    adiciona campo incident_id ao AvaliacaoCreateSerializer e implementa validação e lógica de criação

--- backend/avaliacao/api/serializers/avaliacao_create_serializer.py
+++ backend/avaliacao/api/serializers/avaliacao_create_serializer.py
@@ -1,13 +1,16 @@
+from dw_analytics.models import Incident
 from rest_framework import serializers
 
 from ...models import Avaliacao
 
 
 class AvaliacaoCreateSerializer(serializers.ModelSerializer):
+    incident_id = serializers.IntegerField()
+
     class Meta:
         model = Avaliacao
         fields = [
-            "incident",
+            "incident_id",
             "is_contrato_lancado",
             "is_horas_lancadas",
             "is_has_met_first_response_target",
@@ -18,3 +21,15 @@ class AvaliacaoCreateSerializer(serializers.ModelSerializer):
             "is_cliente_notificado",
             "is_category_correto",
         ]
+
+    def validate_incident_id(self, value):
+        try:
+            Incident.objects.get(id=value)
+        except Incident.DoesNotExist:
+            raise serializers.ValidationError("Incident não encontrado")
+        return value
+
+    def create(self, validated_data):
+        incident_id = validated_data.pop('incident_id')
+        incident = Incident.objects.get(id=incident_id)
+        return Avaliacao.objects.create(incident=incident, **validated_data)
